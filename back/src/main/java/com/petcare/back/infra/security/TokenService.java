09ae9a141a2c9a01b269f45311297af7065a2fd0
martindipeco <martindipeco@gmail.com>
2025-08-19 15:41:28 -0300

    agrego dependencias en pom para auth0 y documentacion

---
+++ back/src/main/java/com/petcare/back/infra/security/TokenService.java
@@ -0,0 +1,20 @@
+package com.petcare.back.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import com.alura.back.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+public class TokenService {
+}
