5efc186baeee00bfb2bc66c0eed1e6e004429e52
JuliaDaniR <julidrz@hotmail.com.ar>
2025-08-28 08:09:56 -0300

    Ordenando el codigo

--- back/src/main/resources/db/V13__create_incidentsImage_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE incident_images (
-                                 id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                 image_name VARCHAR(255),
-                                 image_type VARCHAR(100),
-                                 data LONGBLOB,
-                                 incident_id BIGINT NOT NULL,
-                                 CONSTRAINT fk_image_incident FOREIGN KEY (incident_id) REFERENCES incidents(id) ON DELETE CASCADE
