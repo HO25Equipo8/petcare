d52f65cab7882d9983cdf4648922fab39fbeed77
martindipeco <martindipeco@gmail.com>
2025-08-28 08:26:58 -0300

    borro lineas de debug y agrego lineas de api key maps en ap properties

--- back/src/main/java/com/petcare/back/BackApplication.java
+++ back/src/main/java/com/petcare/back/BackApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackApplication.class, args);
-
-        // Added lines for testing
-        System.out.println("PETCARE_GOOGLE_CLIENT_ID: " + System.getenv("PETCARE_GOOGLE_CLIENT_ID"));
-        System.out.println("PETCARE_GOOGLE_CLIENT_SECRET: " + (System.getenv("PETCARE_GOOGLE_CLIENT_SECRET") != null ? "***PRESENT***" : "***MISSING***"));
-	}
+		SpringApplication.run(BackApplication.class, args);}
 
 }
