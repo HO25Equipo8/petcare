4c94d8f7a87a69f7b5d85416e97f04a05773f35b
martindipeco <martindipeco@gmail.com>
2025-09-15 22:26:10 -0300

    agrego metodo de Leo en PetRepository

--- back/src/main/java/com/petcare/back/repository/PetRepository.java
+++ back/src/main/java/com/petcare/back/repository/PetRepository.java
@@ -1,9 +1,13 @@
 package com.petcare.back.repository;
 
 import com.petcare.back.domain.entity.Pet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    Page<Pet> findAllByOwnerId(Long ownerId, Pageable pageable);
 }
